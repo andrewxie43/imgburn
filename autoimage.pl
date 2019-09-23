bb8794e7d36d76f16c6cc3a8ff4e20ffe3df8c7c
Andrew Xie <andrewxie43@gmail.com>
2019-09-23 08:31:13 -0700

    Cleaned up further, fixed code of syntax errors

--- autoimage.pl
+++ autoimage.pl
@@ -1,43 +1,67 @@
 #! /usr/bin/env perl
 
 use warnings;
-use strict;
 use autodie;
 use File::Temp qw(tempfile tempdir);
 
+#Implements dmesg, so sd card can be swapped without removing the usb adapter.
+
+
 START: #Start Label
 
-my $init = `lsusb`;
+print "\a\aPlease run this script with sudo!\n";
+
+print "Input pwd of img file:\n";
+my $loc = <STDIN>;
+
+print "Reinput pwd of img file:\n";
+my $loc2 = <STDIN>;
+
+if ($loc ne $loc2){
+	print "\n\nDifferent pwds inputted!\n\n";
+	goto START;
+}
+
+MAIN:
+
+my $init = `dmesg | grep -v UFW | tail`; #filter out UFW mlogs
 my $change;
 
 
-print "\nCurrent Devices:\n".$init;
+print "\nCurrent DMESG:\n".$init;
 
 print "\n\nPlease insert device\n\n";
-while (`lsusb` eq $init){
+while (`dmesg | grep -v UFW | tail` eq $init){
 }
+$change = `dmesg | grep -v UFW | tail`;
 
-$change = `lsusb`;
-print "\nDevices Updated:\n".$change."\n";
+$init =~ s/.*\n//; #trim first two lines off init
+$init =~ s/.*\n//;
+
+$change =~ s/\Q$init//; #removes all lines from log before inserting drive
+
+print "changex2:".$change."\n";
+
+$change =~ s/.*\n//; #remove first line with no "sda" info
+
+print "change before trim:".$change."\n";
+
+$change =~ s/[ [0-9.]*] //;
+$change =~ s/:.*//; #isolate drive name (usually sda?)
+
+$change =~ s/^\s+|\s+$//g; #trim whitespace
 
 
+print "\nLocation of Inserted Drive:\n".$change."\n";
 
-my @ia = split(' ',$init); # ia="initial array"
-my @ca = split(' ',$change);
+#assumes that location is /dev/$change
 
 
-my $index = 0;
+system('dd bs=4M if=${loc} of=/dev/${change} conv=fdatasync status=progress');
 
-my @new;
-foreach (@ia){
-	if (not (\$_ eq @ca[$index])){
-		push (@new, $ca[$index]);		
-	}
 
-	$index += 1;
-	
-}
+print "\a\a\aComplete! Restarting program...\n";
 
-print @new;
+goto MAIN;
 
 
